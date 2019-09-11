371f70592dc773d7664c0b442056d33d4104e069
siegelwang <siegelwang@tencent.com>
2019-09-11 15:04:14 +0800

    删除web中多余的indent为2的设置

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -99,9 +99,7 @@
 (use-package web-mode
   :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
   :config
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-css-indent-offset 2))
 
 ;; Format HTML, CSS and JavaScript/JSON
 ;; Install: npm -g install prettier
