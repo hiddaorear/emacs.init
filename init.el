9b0edc16f3c6ca7fdaf76cfbe53b0e36ec2bf7f2
YiFeng Wang <hiddaorear@outlook.com>
2019-09-21 16:02:13 +0800

    添加ReasonML和OCaml支持

--- init.el
+++ init.el
@@ -113,6 +113,9 @@ decrease this. If you experience stuttering, increase this.")
 (require 'init-shell)
 (require 'init-eshell)
 (require 'init-web)
+(require 'init-ocaml)
 
 ;;;;
-(put 'narrow-to-region 'disabled nil)
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
--- lisp/init-ocaml.el
+++ lisp/init-ocaml.el
@@ -1,45 +1,36 @@
-;; company instead of ac-complete
-;; I specifically do not use autocomplete so no (setq merlin-ac-setup 'easy)
-
-;; reason setup
-(defun shell-cmd (cmd)
-  "Returns the stdout output of a shell command or nil if the command returned
-   an error"
-  (car (ignore-errors (apply 'process-lines (split-string cmd)))))
-
-(let* ((refmt-bin (shell-cmd "which refmt"))
-       (merlin-bin (shell-cmd "which ocamlmerlin"))
-       (merlin-base-dir (when merlin-bin
-                          (replace-regexp-in-string "bin/ocamlmerlin$" "" merlin-bin))))
-
-  (when refmt-bin
-    (setq refmt-command refmt-bin))
-
-  (add-hook
-   'reason-mode-hook
-   (lambda ()
-     (add-hook 'before-save-hook 'refmt-before-save nil t)
-     (merlin-mode))))
-
+(use-package tuareg
+        :ensure t
+        :config
+        (add-hook 'tuareg-mode-hook #'electric-pair-local-mode)
+        ;; (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
+        (setq auto-mode-alist
+              (append '(("\\.ml[ily]?$" . tuareg-mode)
+                        ("\\.topml$" . tuareg-mode))
+                      auto-mode-alist)))
 (use-package merlin
+  ;; note: git clone https://github.com/ocaml/merlin to ~/.emacs.d/bootleg
+  :load-path "~/.emacs.d/bootleg/merlin/emacs/"
   :config
-  (setq merlin-command (shell-cmd "which ocamlmerlin"))
   (setq merlin-completion-with-doc t)
 
   ;; Make company aware of merlin
   (with-eval-after-load 'company
     (add-hook 'merlin-mode-hook 'company-mode)
     (add-to-list 'company-backends 'merlin-company-backend))
-
+   ;; Enable auto-complete
+   (setq merlin-use-auto-complete-mode 'easy)
+   ;; Use opam switch to lookup ocamlmerlin binary
+   (setq merlin-command 'opam)
+   (setq merlin-error-after-save nil)
   :bind (:map merlin-mode-map
-         ("M-." . merlin-locate)
-         ("M-," . merlin-pop-stack)
-         ("M-m" . merlin-error-next)
-         ("M-n" . merlin-error-prev)
-         ("C-c C-o" . merlin-occurrences)
-         ("C-c C-j" . merlin-jump)
-         ("C-c i" . merlin-locate-ident)
-         ("C-c C-e" . merlin-iedit-occurrences))
+	      ("M-." . merlin-locate)
+	      ("M-," . merlin-pop-stack)
+	      ("M-m" . merlin-error-next)
+	      ("M-n" . merlin-error-prev)
+	      ("C-c C-o" . merlin-occurrences)
+	      ("C-c C-j" . merlin-jump)
+	      ("C-c i" . merlin-locate-ident)
+	      ("C-c C-e" . merlin-iedit-occurrences))
   :hook
   ;; Start merlin on ml files
   (reason-mode . merlin-mode)
@@ -52,12 +43,11 @@
     (setq utop-command "rtop -emacs")))
 
 (use-package utop
+  ;; note: git clone https://github.com/diml/utop to ~/.emacs.d/bootleg
+  :load-path "~/.emacs.d/bootleg/utop/src/top"
   :hook
   (tuareg-mode . utop-minor-mode)
   (reason-mode . utop-minor-mode))
 
 
 (provide 'init-ocaml)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; init-ocaml.el ends here
